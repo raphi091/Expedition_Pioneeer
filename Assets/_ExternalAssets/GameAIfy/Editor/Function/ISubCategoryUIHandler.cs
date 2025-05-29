692b3c04e7cb4170e3d87d789a1c75fef4a76f3c
raphi091 <kwy8024@gmail.com>
2025-05-29 17:47:36 +0900

    v0.0.1

--- Assets/_ExternalAssets/GameAIfy/Editor/Function/ISubCategoryUIHandler.cs
+++
@@ -1,8 +0,0 @@
-
-namespace GameAIfySDK
-{
-    public interface ISubCategoryUIHandler
-    {
-        void DrawUI();
-    }
-}
--- Assets/_ExternalAssets/GameAIfy/Version/GameAIfyVersion.cs
+++
@@ -1,4 +0,0 @@
-public class GameAIfyVersion
-{
-    public static readonly string VERSION = "GameAIfy_release_v1.1.0";
-}
